fe0417bea4bb1ce4f3a445b9d9ee4ebb66fd4ebf
Arzoka <114647680+Arzoka@users.noreply.github.com>
2023-04-24 14:57:46 +0200

    <3

--- encryptweb/javascript/main.js
+++ encryptweb/javascript/main.js
@@ -8,7 +8,7 @@ const g = "[]";
 const h = ";";
 const i = "3";
 const j = "7";
-const k = "b";
+const k = "↔";
 const l = "X";
 const m = "Z";
 const n = "&";
@@ -26,72 +26,77 @@ const y = "*";
 const z = ".";
 const exlam = "◄";
 
+String.prototype.replaceAll = function(str1, str2, ignore) 
+{
+    return this.replace(new RegExp(str1.replace(/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\<\>\-\&])/g,"\\$&"),(ignore?"gi":"g")),(typeof(str2)=="string")?str2.replace(/\$/g,"$$$$"):str2);
+} 
+
 function dofunkystuff(arg1,string) {
     if (arg1 == "encrypt") {
-        newstring = string.replace("a",a);
-        newstring = newstring.replace("b",b);
-        newstring = newstring.replace("c",c);
-        newstring = newstring.replace("d",d);
-        newstring = newstring.replace("e",e);
-        newstring = newstring.replace("f",f);
-        newstring = newstring.replace("g",g);
-        newstring = newstring.replace("h",h);
-        newstring = newstring.replace("i",i);
-        newstring = newstring.replace("j",j);
-        newstring = newstring.replace("k",k);
-        newstring = newstring.replace("l",l);
-        newstring = newstring.replace("m",m);
-        newstring = newstring.replace("n",n);
-        newstring = newstring.replace("o",o);
-        newstring = newstring.replace("p",p);
-        newstring = newstring.replace("q",q);
-        newstring = newstring.replace("r",r);
-        newstring = newstring.replace("s",s);
-        newstring = newstring.replace("t",t);
-        newstring = newstring.replace("u",u);
-        newstring = newstring.replace("v",v);
-        newstring = newstring.replace("w",w);
-        newstring = newstring.replace("x",x);
-        newstring = newstring.replace("y",y);
-        newstring = newstring.replace("z",z);
-        newstring = newstring.replace("!",exlam);
+        newstring = string.replaceAll("a",a);
+        newstring = newstring.replaceAll("b",b);
+        newstring = newstring.replaceAll("c",c);
+        newstring = newstring.replaceAll("d",d);
+        newstring = newstring.replaceAll("e",e);
+        newstring = newstring.replaceAll("f",f);
+        newstring = newstring.replaceAll("g",g);
+        newstring = newstring.replaceAll("h",h);
+        newstring = newstring.replaceAll("i",i);
+        newstring = newstring.replaceAll("j",j);
+        newstring = newstring.replaceAll("k",k);
+        newstring = newstring.replaceAll("l",l);
+        newstring = newstring.replaceAll("m",m);
+        newstring = newstring.replaceAll("n",n);
+        newstring = newstring.replaceAll("o",o);
+        newstring = newstring.replaceAll("p",p);
+        newstring = newstring.replaceAll("q",q);
+        newstring = newstring.replaceAll("r",r);
+        newstring = newstring.replaceAll("s",s);
+        newstring = newstring.replaceAll("t",t);
+        newstring = newstring.replaceAll("u",u);
+        newstring = newstring.replaceAll("v",v);
+        newstring = newstring.replaceAll("w",w);
+        newstring = newstring.replaceAll("x",x);
+        newstring = newstring.replaceAll("y",y);
+        newstring = newstring.replaceAll("z",z);
+        newstring = newstring.replaceAll("!",exlam);
         return newstring;
     }
 
     if (arg1 == "decrypt") {
-        newstring = string.replace(a,"a");
-        newstring = newstring.replace(b,"b");
-        newstring = newstring.replace(c,"c");
-        newstring = newstring.replace(d,"d");
-        newstring = newstring.replace(e,"e");
-        newstring = newstring.replace(f,"f");
-        newstring = newstring.replace(g,"g");
-        newstring = newstring.replace(h,"h");
-        newstring = newstring.replace(i,"i");
-        newstring = newstring.replace(j,"j");
-        newstring = newstring.replace(k,"k");
-        newstring = newstring.replace(l,"l");
-        newstring = newstring.replace(m,"m");
-        newstring = newstring.replace(n,"n");
-        newstring = newstring.replace(o,"o");
-        newstring = newstring.replace(p,"p");
-        newstring = newstring.replace(q,"q");
-        newstring = newstring.replace(r,"r");
-        newstring = newstring.replace(s,"s");
-        newstring = newstring.replace(t,"t");
-        newstring = newstring.replace(u,"u");
-        newstring = newstring.replace(v,"v");
-        newstring = newstring.replace(w,"w");
-        newstring = newstring.replace(x,"x");
-        newstring = newstring.replace(y,"y");
-        newstring = newstring.replace(z,"z");
-        newstring = newstring.replace(exlam,"!");
+        newstring = string.replaceAll(a,"a");
+        newstring = newstring.replaceAll(b,"b");
+        newstring = newstring.replaceAll(c,"c");
+        newstring = newstring.replaceAll(d,"d");
+        newstring = newstring.replaceAll(e,"e");
+        newstring = newstring.replaceAll(f,"f");
+        newstring = newstring.replaceAll(g,"g");
+        newstring = newstring.replaceAll(h,"h");
+        newstring = newstring.replaceAll(i,"i");
+        newstring = newstring.replaceAll(j,"j");
+        newstring = newstring.replaceAll(k,"k");
+        newstring = newstring.replaceAll(l,"l");
+        newstring = newstring.replaceAll(m,"m");
+        newstring = newstring.replaceAll(n,"n");
+        newstring = newstring.replaceAll(o,"o");
+        newstring = newstring.replaceAll(p,"p");
+        newstring = newstring.replaceAll(q,"q");
+        newstring = newstring.replaceAll(r,"r");
+        newstring = newstring.replaceAll(s,"s");
+        newstring = newstring.replaceAll(t,"t");
+        newstring = newstring.replaceAll(u,"u");
+        newstring = newstring.replaceAll(v,"v");
+        newstring = newstring.replaceAll(w,"w");
+        newstring = newstring.replaceAll(x,"x");
+        newstring = newstring.replaceAll(y,"y");
+        newstring = newstring.replaceAll(z,"z");
+        newstring = newstring.replaceAll(exlam,"!");
         return newstring;
     }
 }
 
 document.getElementById("encrypt-b").onclick = function() {
-    am = document.getElementById("inpamount").value;
+    am = 1;
     if (am > 0) {
         for (let i = 0; i < am; i++) {
             if (i == 0) {
@@ -108,7 +113,7 @@ document.getElementById("encrypt-b").onclick = function() {
 }
 
 document.getElementById("decrypt-b").onclick = function() {
-    am = document.getElementById("inpamount").value;
+    am = 1;
     if (am > 0) {
         for (let i = 0; i < am; i++) {
             if (i == 0) {
