cbede7a68ffb1116a085e271ceac5efb2750d6a6
Arzoka <114647680+Arzoka@users.noreply.github.com>
2023-04-24 15:38:48 +0200

    added json

--- encryptweb/javascript/main.js
+++ encryptweb/javascript/main.js
@@ -1,130 +1,108 @@
-const a = "{#*}";
-const b = "DE";
-const c = "92";
-const d = "VA";
-const e = "KJ";
-const f = "L9";
-const g = "[]";
-const h = ";";
-const i = "3";
-const j = "7";
-const k = "↔";
-const l = "X";
-const m = "Z";
-const n = "&";
-const o = "$";
-const p = "%";
-const q = "←";
-const r = "?";
-const s = "@";
-const t = "-";
-const u = "+";
-const v = "|";
-const w = "O";
-const x = "x";
-const y = "*";
-const z = ".";
-const exlam = "◄";
-
 String.prototype.replaceAll = function(str1, str2, ignore) 
 {
     return this.replace(new RegExp(str1.replace(/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\<\>\-\&])/g,"\\$&"),(ignore?"gi":"g")),(typeof(str2)=="string")?str2.replace(/\$/g,"$$$$"):str2);
-} 
-
-function dofunkystuff(arg1,string) {
-    if (arg1 == "encrypt") {
-        newstring = string.replaceAll("a",a);
-        newstring = newstring.replaceAll("b",b);
-        newstring = newstring.replaceAll("c",c);
-        newstring = newstring.replaceAll("d",d);
-        newstring = newstring.replaceAll("e",e);
-        newstring = newstring.replaceAll("f",f);
-        newstring = newstring.replaceAll("g",g);
-        newstring = newstring.replaceAll("h",h);
-        newstring = newstring.replaceAll("i",i);
-        newstring = newstring.replaceAll("j",j);
-        newstring = newstring.replaceAll("k",k);
-        newstring = newstring.replaceAll("l",l);
-        newstring = newstring.replaceAll("m",m);
-        newstring = newstring.replaceAll("n",n);
-        newstring = newstring.replaceAll("o",o);
-        newstring = newstring.replaceAll("p",p);
-        newstring = newstring.replaceAll("q",q);
-        newstring = newstring.replaceAll("r",r);
-        newstring = newstring.replaceAll("s",s);
-        newstring = newstring.replaceAll("t",t);
-        newstring = newstring.replaceAll("u",u);
-        newstring = newstring.replaceAll("v",v);
-        newstring = newstring.replaceAll("w",w);
-        newstring = newstring.replaceAll("x",x);
-        newstring = newstring.replaceAll("y",y);
-        newstring = newstring.replaceAll("z",z);
-        newstring = newstring.replaceAll("!",exlam);
-        return newstring;
-    }
-
-    if (arg1 == "decrypt") {
-        newstring = string.replaceAll(a,"a");
-        newstring = newstring.replaceAll(b,"b");
-        newstring = newstring.replaceAll(c,"c");
-        newstring = newstring.replaceAll(d,"d");
-        newstring = newstring.replaceAll(e,"e");
-        newstring = newstring.replaceAll(f,"f");
-        newstring = newstring.replaceAll(g,"g");
-        newstring = newstring.replaceAll(h,"h");
-        newstring = newstring.replaceAll(i,"i");
-        newstring = newstring.replaceAll(j,"j");
-        newstring = newstring.replaceAll(k,"k");
-        newstring = newstring.replaceAll(l,"l");
-        newstring = newstring.replaceAll(m,"m");
-        newstring = newstring.replaceAll(n,"n");
-        newstring = newstring.replaceAll(o,"o");
-        newstring = newstring.replaceAll(p,"p");
-        newstring = newstring.replaceAll(q,"q");
-        newstring = newstring.replaceAll(r,"r");
-        newstring = newstring.replaceAll(s,"s");
-        newstring = newstring.replaceAll(t,"t");
-        newstring = newstring.replaceAll(u,"u");
-        newstring = newstring.replaceAll(v,"v");
-        newstring = newstring.replaceAll(w,"w");
-        newstring = newstring.replaceAll(x,"x");
-        newstring = newstring.replaceAll(y,"y");
-        newstring = newstring.replaceAll(z,"z");
-        newstring = newstring.replaceAll(exlam,"!");
-        return newstring;
-    }
 }
 
-document.getElementById("encrypt-b").onclick = function() {
-    am = 1;
-    if (am > 0) {
-        for (let i = 0; i < am; i++) {
-            if (i == 0) {
-                document.getElementById("endresult").innerHTML = dofunkystuff("encrypt",document.getElementById("inp").value);
-            }
-            else{
-                document.getElementById("endresult").innerHTML = dofunkystuff("encrypt",document.getElementById("endresult").innerHTML);
-            }
+fetch('enc.json')
+  .then(response => response.json())
+  .then(data => {
+
+    function dofunkystuff(arg1,string) {
+        if (arg1 == "encrypt") {
+            newstring = string.replaceAll("a",data.encr.a);
+            newstring = newstring.replaceAll("b",data.encr.b);
+            newstring = newstring.replaceAll("c",data.encr.c);
+            newstring = newstring.replaceAll("d",data.encr.d);
+            newstring = newstring.replaceAll("e",data.encr.e);
+            newstring = newstring.replaceAll("f",data.encr.f);
+            newstring = newstring.replaceAll("g",data.encr.g);
+            newstring = newstring.replaceAll("h",data.encr.h);
+            newstring = newstring.replaceAll("i",data.encr.i);
+            newstring = newstring.replaceAll("j",data.encr.j);
+            newstring = newstring.replaceAll("k",data.encr.k);
+            newstring = newstring.replaceAll("l",data.encr.l);
+            newstring = newstring.replaceAll("m",data.encr.m);
+            newstring = newstring.replaceAll("n",data.encr.n);
+            newstring = newstring.replaceAll("o",data.encr.o);
+            newstring = newstring.replaceAll("p",data.encr.p);
+            newstring = newstring.replaceAll("q",data.encr.q);
+            newstring = newstring.replaceAll("r",data.encr.r);
+            newstring = newstring.replaceAll("s",data.encr.s);
+            newstring = newstring.replaceAll("t",data.encr.t);
+            newstring = newstring.replaceAll("u",data.encr.u);
+            newstring = newstring.replaceAll("v",data.encr.v);
+            newstring = newstring.replaceAll("w",data.encr.w);
+            newstring = newstring.replaceAll("x",data.encr.x);
+            newstring = newstring.replaceAll("y",data.encr.y);
+            newstring = newstring.replaceAll("z",data.encr.z);
+            newstring = newstring.replaceAll("!",data.encr.exlam);
+            return newstring;
+        }
+
+        if (arg1 == "decrypt") {
+            newstring = string.replaceAll(data.encr.a,"a");
+            newstring = newstring.replaceAll(data.encr.b,"b");
+            newstring = newstring.replaceAll(data.encr.c,"c");
+            newstring = newstring.replaceAll(data.encr.d,"d");
+            newstring = newstring.replaceAll(data.encr.e,"e");
+            newstring = newstring.replaceAll(data.encr.f,"f");
+            newstring = newstring.replaceAll(data.encr.g,"g");
+            newstring = newstring.replaceAll(data.encr.h,"h");
+            newstring = newstring.replaceAll(data.encr.i,"i");
+            newstring = newstring.replaceAll(data.encr.j,"j");
+            newstring = newstring.replaceAll(data.encr.k,"k");
+            newstring = newstring.replaceAll(data.encr.l,"l");
+            newstring = newstring.replaceAll(data.encr.m,"m");
+            newstring = newstring.replaceAll(data.encr.n,"n");
+            newstring = newstring.replaceAll(data.encr.o,"o");
+            newstring = newstring.replaceAll(data.encr.p,"p");
+            newstring = newstring.replaceAll(data.encr.q,"q");
+            newstring = newstring.replaceAll(data.encr.r,"r");
+            newstring = newstring.replaceAll(data.encr.s,"s");
+            newstring = newstring.replaceAll(data.encr.t,"t");
+            newstring = newstring.replaceAll(data.encr.u,"u");
+            newstring = newstring.replaceAll(data.encr.v,"v");
+            newstring = newstring.replaceAll(data.encr.w,"w");
+            newstring = newstring.replaceAll(data.encr.x,"x");
+            newstring = newstring.replaceAll(data.encr.y,"y");
+            newstring = newstring.replaceAll(data.encr.z,"z");
+            newstring = newstring.replaceAll(data.encr.exlam,"!");
+            return newstring;
         }
     }
-    else{
-        document.getElementById("endresult").innerHTML = "ERROR! Please specify an amount of times to encrypt!";
-    }
-}
 
-document.getElementById("decrypt-b").onclick = function() {
-    am = 1;
-    if (am > 0) {
-        for (let i = 0; i < am; i++) {
-            if (i == 0) {
-                document.getElementById("endresult").innerHTML = dofunkystuff("decrypt",document.getElementById("inp").value);
-            }
-            else{
-                document.getElementById("endresult").innerHTML = dofunkystuff("decrypt",document.getElementById("endresult").innerHTML);
+    document.getElementById("encrypt-b").onclick = function() {
+        am = 1;
+        if (am > 0) {
+            for (let i = 0; i < am; i++) {
+                if (i == 0) {
+                    document.getElementById("endresult").innerHTML = dofunkystuff("encrypt",document.getElementById("inp").value);
+                }
+                else{
+                    document.getElementById("endresult").innerHTML = dofunkystuff("encrypt",document.getElementById("endresult").innerHTML);
+                }
             }
         }
+        else{
+            document.getElementById("endresult").innerHTML = "ERROR! Please specify an amount of times to encrypt!";
+        }
     }
-    else{
-        document.getElementById("endresult").innerHTML = "ERROR! Please specify an amount of times to decrypt!";
+
+    document.getElementById("decrypt-b").onclick = function() {
+        am = 1;
+        if (am > 0) {
+            for (let i = 0; i < am; i++) {
+                if (i == 0) {
+                    document.getElementById("endresult").innerHTML = dofunkystuff("decrypt",document.getElementById("inp").value);
+                }
+                else{
+                    document.getElementById("endresult").innerHTML = dofunkystuff("decrypt",document.getElementById("endresult").innerHTML);
+                }
+            }
+        }
+        else{
+            document.getElementById("endresult").innerHTML = "ERROR! Please specify an amount of times to decrypt!";
+        }
     }
-}
+
+})
